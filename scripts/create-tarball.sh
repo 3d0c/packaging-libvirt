2555c6d0014663cc3f530b730cf45e29dc84e11e
3d0c <alex.s.contact@gmail.com>
2026-03-18 11:45:31 +0300

    add meson subprojects

--- scripts/create-tarball.sh
+++ scripts/create-tarball.sh
@@ -19,6 +19,9 @@ git clone --filter=blob:none "$UPSTREAM_REPO" "$WORKDIR/src"
 pushd "$WORKDIR/src" > /dev/null
 echo "==> Checking out ${COMMIT}..."
 git checkout "$COMMIT"
+echo "==> Downloading meson subprojects..."
+meson subprojects download || true
+
 popd > /dev/null
 
 echo "==> Preparing source tree as ${EXTRACT_DIR}/..."
